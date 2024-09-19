5eb8dd29d221667ae7e4dfbd0f02802c2edb490a
adelegem <135280669+adelegem@users.noreply.github.com>
2024-09-20 09:17:11 +1000

    update create_multiband_image funx

--- R/create_multiband_image.R
+++ R/create_multiband_image.R
@@ -1,27 +1,31 @@
 #' Create multiband image
 #'
-#' A description of what your function does.
+#' Combines single reflectance band tifs into a multiband image.
 #'
-#' @param mosaics_dir Description of the parameter x.
-#' @param desired_band_order description
-#' @return  combined tif image
+#' @param mosaics_dir Folder which contains single band tifs to be combined
+#' @param desired_band_order Order of files to be combined. should be in wavelength order. provide list of file basenames in correct order
+#' @param output_dir Folder to store combined tif file
+#' @return  combined tif file
+#' @examples
+#' create_multiband_image('001_site_mosaics', c('blue', 'green', 'red', 'red_edge', 'nir'), 'data_out/001_site')
 #' @export
-create_multiband_image <- function(mosaics_dir, desired_band_order){
+#' @import tools
+#' @import terra
+#'
+create_multiband_image <- function(mosaics_dir, desired_band_order, output_dir){
   # folder list | recursive = won't pick folders within folders
   folders <- list.dirs(mosaics_dir, full.names = FALSE, recursive = FALSE)
-  # need to make this part an argument e.g. option to exclude certain folders or include certain folders
-  folders <- folders[folders != "point_clouds"]
 
   ## NOTE: spectral band image tif file names must be named after their band (e.g., blue, nir, etc),
   #  otherwise change 'desired_band_order' to match file names
-  #  should be combined in wavelength order, esp for biodivmapR processes (i.e. as above)
+  #  should be combined in wavelength order
 
   # loop thru each folder
   for (folder in folders) {
     # create path
     folder_path <- file.path(mosaics_dir, folder)
 
-    # list of tif files | \\. represents . (dots need to be escaped w \, \ need to be escaped with  \). $means at end of file name/string
+    # list of tif files
     tif_files <- list.files(folder_path, pattern = "\\.tif$", full.names = TRUE)
 
     # load as raster
@@ -37,17 +41,13 @@ create_multiband_image <- function(mosaics_dir, desired_band_order){
     # reorder the bands based on the desired band order
     combined_image <- combined_image[[match(desired_band_order, band_names)]]
 
-    #create output directory folder if it doesn't exist
-    output_dir <- file.path("data_out/combined_rasters", substr(basename(mosaics_dir), 1, 4))
-    if (!dir.exists(output_dir)) {
-      dir.create(output_dir, recursive = TRUE)
-    }
-
     # create output file as .tif and as .envi
     output_filename <- file.path(output_dir, paste0(folder, "_combined_image"))
     # write .tif file
     terra::writeRaster(combined_image, filename = paste0(output_filename, '.tif'),
                        filetype = "GTiff", gdal = c("INTERLEAVE=BAND"), overwrite = TRUE)
+
+    # plot image - for checking
     plot(combined_image)
   }
 }
