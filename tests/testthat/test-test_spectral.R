cb8ae52aa6adf5a0b9ebf28c1e644642eddab348
Will Cornwell <w.cornwell@unsw.edu.au>
2024-11-15 11:53:13 +1100

    maybe a real test

--- tests/testthat/test-test_spectral.R
+++ tests/testthat/test-test_spectral.R
@@ -1,3 +1,14 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("calculate_cv works", {
+  set.seed(123)
+  df <- data.frame(
+  site_name = rep(c("site_one", "site_two", "site_three", "site_four"), each = 5000),
+  aoi_id = 1,
+  blue = runif(20000, min = 0, max = 1),
+  green = runif(20000, min = 0, max = 1),
+  red = runif(20000, min = 0, max = 1),
+  red_edge = runif(20000, min = 0, max = 1),
+  nir = runif(20000, min = 0, max = 1))
+  pixelvalues <- calculate_cv(df,
+  wavelengths = c('blue','green','red','red_edge','nir'),
+  rarefaction = TRUE, min_points = 5000, n = 999)
 })
