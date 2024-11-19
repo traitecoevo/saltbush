0482835aa0dc002f8342d777a70097091dcd71a2
Will Cornwell <w.cornwell@unsw.edu.au>
2024-11-19 14:28:43 +1100

    another test

---
+++ tests/testthat/test-field_diversity.R
@@ -0,0 +1,10 @@
+ausplot.pi.data <- ausplotsR::get_ausplots(my.Plot_IDs=
+                                             c("NTASTU0002"), veg.PI=TRUE)$veg.PI
+
+test_that('field_diversity works', {
+field_diversity <- calculate_field_diversity(ausplot.pi.data)
+expect_true(
+field_diversity$field_diversity$species_richness==
+  length(na.omit(unique(ausplot.pi.data$standardised_name)))
+)
+})
