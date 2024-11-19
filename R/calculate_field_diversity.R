1c528e1d9669eac3e43e48be551da28fa20630c8
Will Cornwell <w.cornwell@unsw.edu.au>
2024-11-19 13:56:59 +1100

    seeing if native pipe breaks things

--- R/calculate_field_diversity.R
+++ R/calculate_field_diversity.R
@@ -3,10 +3,10 @@
 #' @param survey_data a df obtained from ausplotsR package with survey data for appropriate sites
 #' @return a list, containing 'field diversity' - values of species richness, shannon's index, simpsons index, exponential shannon's, inverse simpson, pielou's evenness, and 'community matrices'
 #' @export
-#' @import ausplotsR
 #' @examples
-#' my.data <- ausplotsR::get_ausplots(my.Plot_IDs=c("SATFLB0004", "QDAMGD0022", "NTASTU0002"),veg.PI=TRUE)$veg.PI
-#' field_diversity <- calculate_field_diversity(my.data)
+#' ausplot.pi.data <- ausplotsR::get_ausplots(my.Plot_IDs=
+#'     c("SATFLB0004", "QDAMGD0022", "NTASTU0002"), veg.PI=TRUE)$veg.PI
+#' field_diversity <- calculate_field_diversity(ausplot.pi.data)
 #'
 
 calculate_field_diversity <- function(survey_data){
@@ -23,20 +23,20 @@ calculate_field_diversity <- function(survey_data){
   # loop thru each unique site
   for (site in ausplot_sites) {
     # Filter data for the current site
-    site_survey_data <- survey_data %>%
+    site_survey_data <- survey_data |>
       dplyr::filter(site_unique == site)
 
-    subplot_diversity <- site_survey_data %>%
-      tidyr::drop_na(standardised_name) %>%
-      dplyr::filter(!standardised_name %in% c('Dead grass', 'Dead shrub')) %>%
-      #group_by(subplot_id) %>%
+    subplot_diversity <- site_survey_data |>
+      tidyr::drop_na(standardised_name) |>
+      dplyr::filter(!standardised_name %in% c('Dead grass', 'Dead shrub')) |>
+      #group_by(subplot_id) |>
       dplyr::summarise(species_richness = dplyr::n_distinct(standardised_name))
 
-    community_matrix <- site_survey_data %>%
-      tidyr::drop_na(standardised_name) %>%
-      dplyr::filter(!standardised_name %in% c('Dead grass', 'Dead shrub')) %>%
-      #count(subplot_id, standardised_name) %>%
-      dplyr::count(standardised_name) %>%
+    community_matrix <- site_survey_data |>
+      tidyr::drop_na(standardised_name) |>
+      dplyr::filter(!standardised_name %in% c('Dead grass', 'Dead shrub')) |>
+      #count(subplot_id, standardised_name) |>
+      dplyr::count(standardised_name) |>
       tidyr::spread(standardised_name, n, fill = 0)
 
     # store the community matrix  in the list
@@ -47,7 +47,7 @@ calculate_field_diversity <- function(survey_data){
     simpson_diversity <- vegan::diversity(community_matrix[, -1], index = "simpson")
     inv_simpson <- vegan::diversity(community_matrix[, -1], index = 'invsimpson')
 
-    subplot_diversity <- subplot_diversity %>%
+    subplot_diversity <- subplot_diversity |>
       dplyr::mutate(shannon_diversity = shannon_diversity,
              simpson_diversity = simpson_diversity,
              pielou_evenness = shannon_diversity / log(species_richness),
